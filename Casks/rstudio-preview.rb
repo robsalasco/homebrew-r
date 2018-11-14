63ed701efdc5ac67e8cb5868a7440c52e11631ee
Roberto Salas <wala.rsalas@gmail.com>
2018-11-14 09:25:29 -0300

    Update and rename rstudio.rb to rstudio-preview.rb

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,10 +1,10 @@
-cask 'rstudio' do
+cask 'rstudio-preview' do
   version '1.2.1114'
   sha256 '45591a10b212a251ae83043576ff8198ed750361935c9be66503cfacab6ea2bc'
 
   # rstudio.org was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
-  appcast 'https://www.rstudio.com/products/rstudio/download/'
+  appcast 'http://rstudio.com/products/rstudio/download/preview/'
   name 'RStudio'
   homepage 'https://www.rstudio.com/'
 
